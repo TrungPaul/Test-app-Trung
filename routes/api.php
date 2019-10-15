0257b524a4efe653960bb07c1c0f3e73ff14cd2e
TrungPaul <trungmadrid@gmail.com>
2019-10-15 10:01:32 +0700

    fix hard code and router

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::get('project/list','ProjectController@index');
+Route::get('projects','ProjectController@index');
 Route::post('project/add','ProjectController@store');
 
 
