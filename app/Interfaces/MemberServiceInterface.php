e53851acbd1e21ee3885311bac20daf6b1595c39
TrungPaul <trungmadrid@gmail.com>
2019-10-16 15:18:05 +0700

    up

---
+++ app/Interfaces/MemberServiceInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Interfaces;
+
+interface MemberServiceInterface
+{
+    public function showMember();
+    public function addMember($input);
+    public function editMember($input, $memberId);
+    public function removeMember($memberId);
+}
---
+++ app/Repositories/MemberRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\MemberServiceInterface;
+use App\Member;
+
+class MemberRepository implements MemberServiceInterface
+{
+
+    public function showMember()
+    {
+        // TODO: Implement showMember() method.
+    }
+
+    public function addMember($input)
+    {
+        //
+    }
+
+    public function editMember($input, $memberId)
+    {
+        // TODO: Implement editMember() method.
+    }
+
+    public function removeMember($memberId)
+    {
+        // TODO: Implement removeMember() method.
+    }
+}
