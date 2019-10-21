a614484b1101444651b2d3ced60d2c681199c7f8
TrungPaul <trungmadrid@gmail.com>
2019-10-21 09:57:54 +0700

    fix coding style

--- app/Repositories/MemberProjectRepository.php
+++ app/Repositories/MemberProjectRepository.php
@@ -11,21 +11,7 @@ class MemberProjectRepository implements ProjectOfMemberServiceInterface
     public function showProjectOfMember($projectId)
     {
         $listMemberOfProject = ProjectAndMember::select('member_id')->where('project_id', $projectId)->get()->pluck('member_id')->toArray();
+
         return $member = Member::whereIn('id', $listMemberOfProject)->get();
-    }
-
-    public function addMemberIntoProject($input)
-    {
-        // TODO: Implement addMemberIntoProject() method.
-    }
-
-    public function editMemberInProject($input, $projectId, $memberId)
-    {
-        // TODO: Implement editMemberInProject() method.
-    }
-
-    public function removeMemberInProject($projectId, $memberId)
-    {
-        // TODO: Implement removeMemberInProject() method.
     }
 }
